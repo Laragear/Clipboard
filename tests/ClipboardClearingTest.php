16bd693e67be4f036ec85a5f2284146a01555f66
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2023-03-12 23:54:18 -0300

    Fixes Clipboard clearing test environment.

--- tests/ClipboardClearingTest.php
+++ tests/ClipboardClearingTest.php
@@ -9,13 +9,25 @@ use Illuminate\Events\Dispatcher;
 use Illuminate\Foundation\Http\Events\RequestHandled;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Env;
 use Laragear\Clipboard\Facades\Clipboard;
 use Symfony\Component\Console\Input\StringInput;
 
 class ClipboardClearingTest extends TestCase
 {
+    protected function tearDown(): void
+    {
+        Env::getRepository()->set('APP_RUNNING_IN_CONSOLE', true);
+
+        parent::tearDown();
+    }
+
     public function test_clears_clipboard_after_response_sent(): void
     {
+        Env::getRepository()->set('APP_RUNNING_IN_CONSOLE', false);
+
+        $this->refreshApplication();
+
         Clipboard::copy('foo');
 
         $this->app->make(Dispatcher::class)->dispatch(RequestHandled::class, [
@@ -28,6 +40,10 @@ class ClipboardClearingTest extends TestCase
 
     public function test_clears_clipboard_after_command_finishes(): void
     {
+        Env::getRepository()->set('APP_RUNNING_IN_CONSOLE', true);
+
+        $this->refreshApplication();
+
         Clipboard::copy('foo');
 
         $this->app->make(Dispatcher::class)->dispatch(CommandFinished::class, [
