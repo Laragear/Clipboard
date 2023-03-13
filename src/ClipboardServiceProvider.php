99a22b53c3ceb7cdae539d78766c4543bea0d217
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2023-03-12 23:41:18 -0300

    Changes Clipboard clearing procedure to use Laravel Event handler.

--- src/ClipboardServiceProvider.php
+++ src/ClipboardServiceProvider.php
@@ -2,34 +2,34 @@
 
 namespace Laragear\Clipboard;
 
-use Illuminate\Contracts\Http\Kernel as HttpKernelContract;
-use Illuminate\Foundation\Http\Kernel as LaravelHttpKernel;
+use Illuminate\Console\Events\CommandFinished;
+use Illuminate\Events\Dispatcher as EventContract;
+use Illuminate\Foundation\Http\Events\RequestHandled;
 use Illuminate\Support\ServiceProvider;
+use function app;
 
 class ClipboardServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register(): void
     {
-        $this->app->singleton(Clipboard::class, static function (): Clipboard {
-            return new Clipboard();
-        });
+        $this->app->singleton(Clipboard::class);
     }
 
     /**
      * Bootstrap any application services.
      *
-     * @param  \Illuminate\Contracts\Http\Kernel  $kernel
+     * @param  \Illuminate\Events\Dispatcher  $events
      * @return void
      */
-    public function boot(HttpKernelContract $kernel): void
+    public function boot(EventContract $events): void
     {
-        if ($kernel instanceof LaravelHttpKernel) {
-            $kernel->pushMiddleware(Http\Middleware\ClearClipboard::class);
-        }
+        $event = $this->app->runningInConsole() ? CommandFinished::class : RequestHandled::class;
+
+        $events->listen($event, static function () {
+            app(Clipboard::class)->clear();
+        });
     }
 }
---
+++ tests/ClipboardClearingTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Console\BufferedConsoleOutput;
+use Illuminate\Console\Events\CommandFinished;
+use Illuminate\Console\OutputStyle;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Foundation\Http\Events\RequestHandled;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Laragear\Clipboard\Facades\Clipboard;
+use Symfony\Component\Console\Input\StringInput;
+
+class ClipboardClearingTest extends TestCase
+{
+    public function test_clears_clipboard_after_response_sent(): void
+    {
+        Clipboard::copy('foo');
+
+        $this->app->make(Dispatcher::class)->dispatch(RequestHandled::class, [
+            new Request(),
+            new Response(),
+        ]);
+
+        static::assertNull(Clipboard::paste());
+    }
+
+    public function test_clears_clipboard_after_command_finishes(): void
+    {
+        Clipboard::copy('foo');
+
+        $this->app->make(Dispatcher::class)->dispatch(CommandFinished::class, [
+            'test',
+            new StringInput(''),
+            new OutputStyle(new StringInput(''), new BufferedConsoleOutput()),
+            0
+        ]);
+
+        static::assertNull(Clipboard::paste());
+    }
+}
